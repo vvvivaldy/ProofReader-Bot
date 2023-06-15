f2a2de4b10d96c101bdae04e7a8f80daac100867
Nikita_Gutenev <gutenev229@gmail.com>
2023-06-15 14:22:55 +0300

    фикс связанный с PAYMENTS_TOKEN

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def descr_func(message: types.Message):
 # Хендлер Покупки подписки
 @dp.message_handler(Text(equals="Оформить подписку"))
 async def buy(message: types.Message):
-    if os.getenv('PAYMENTS_TOKEN').split(":")[1] == "TEST":
+    if (PAYMENTS_TOKEN:=os.getenv('PAYMENTS_TOKEN')).split(":")[1] == "TEST":
         await bot.send_message(message.chat.id,
                                "Тестовый платеж")
     await bot.send_invoice(message.chat.id,
