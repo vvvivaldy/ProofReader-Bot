5cde973db7d631119121d7f397092ba69f8b4a48
Nikita_Gutenev <gutenev229@gmail.com>
2023-06-15 17:30:58 +0300

    в связи с изменением принятия и шифровки api - пофиксил получение баланса. Все работает

--- main.py
+++ main.py
@@ -252,10 +252,9 @@ async def balance_func(message: types.Message):
         conn = sqlite3.connect('db/database.db')
         cursor = conn.cursor()
         data = cursor.execute('SELECT api_secret, api_key FROM users WHERE user_id=?;', (message.from_user.id,)).fetchone()
-        print(data)
         session = HTTP(
-            api_key=decrypted_key.decode('utf-8'),
-            api_secret=decrypted_secret.decode('utf-8')
+            api_key=decrypt_api(data[1][2:-1]),
+            api_secret=decrypt_api(data[0][2:-1])
         )
         
         wallet_balance_data = session.get_wallet_balance(accountType="UNIFIED")["result"]["list"][0]
