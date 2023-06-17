840cf1a5b74b3bb5f8431aed218ffc932ecbf7f4
Harpy <gutenev229@gmail.com>
2023-06-17 21:41:26 +0300

    добавил команду входа в админ панель

--- handlers/C_admin_panel_handlers.py
+++ handlers/C_admin_panel_handlers.py
@@ -1,1 +1,12 @@
 from handlers.B_basic_function_handlers import *
+
+admin_ids = [os.getenv('NIKITA_ID'),os.getenv('MISHA_ID'),os.getenv('ROMA_ID')]
+
+
+@dp.message_handler(commands=['ADMINPANEL'])
+async def admin_validate(message: types.Message):
+    if str(message.from_user.id) in admin_ids:
+        print(f'Зашел админ {message.from_user.id}')
+    else:
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Мы не предусмотрели данный запрос. Повторите попытку.")
