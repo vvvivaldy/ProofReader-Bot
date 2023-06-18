6657441e562351d5905dac0ada76cb12d3372f16
Harpy <gutenev229@gmail.com>
2023-06-18 12:26:37 +0300

    добавил функцию валидации на администратора

--- handlers/C_admin_panel_handlers.py
+++ handlers/C_admin_panel_handlers.py
@@ -2,10 +2,13 @@ from handlers.B_basic_function_handlers import *
 
 admin_ids = [os.getenv('NIKITA_ID'),os.getenv('MISHA_ID'),os.getenv('ROMA_ID')]
 
+async def admin_validate(message: types.Message):
+    if str(message.from_user.id) in admin_ids: return True
+    return False
 
 @dp.message_handler(commands=['ADMINPANEL'])
-async def admin_validate(message: types.Message):
-    if str(message.from_user.id) in admin_ids:
+async def admin_check(message: types.Message):
+    if admin_validate(message):
         await bot.send_message(chat_id=message.from_user.id,
                                text='Привет, Хозяин...',
                                reply_markup=kb_admin)
@@ -15,7 +18,8 @@ async def admin_validate(message: types.Message):
         
 @dp.message_handler(Text(equals='Статистика'))
 async def _(message: types.Message):
-    pass
+
+    await bot.send_photo(chat_id=message.from_user.bot)
 
 
 @dp.message_handler(Text(equals='Цены'))
