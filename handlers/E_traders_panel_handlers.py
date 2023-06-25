845e7231f32a7b467814ab9209c3a1e59d0b1f2c
Harpy <gutenev229@gmail.com>
2023-06-25 14:57:48 +0300

    добавил валидацию трейдера в хенделры

--- handlers/E_traders_panel_handlers.py
+++ handlers/E_traders_panel_handlers.py
@@ -3,21 +3,34 @@ from callbacks.trader_callbacks import *
 
 @dp.message_handler(Text(equals='Ключи'))
 async def keys(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text="Выберите действие",
-                           reply_markup=kb_keys)
+    if trader_validate(message.from_user.id):
+        await bot.send_message(chat_id=message.from_user.id,
+                            text="Выберите действие",
+                            reply_markup=kb_keys)
+    else:
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Мы не предусмотрели данный запрос. Повторите попытку.")
 
 
 @dp.message_handler(Text(equals='Вернуться'))
 async def keys(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text="Вы вернулись в меню",
-                           reply_markup=kb_trader)
-    
+    if trader_validate(message.from_user.id):
+        await bot.send_message(chat_id=message.from_user.id,
+                            text="Вы вернулись в меню",
+                            reply_markup=kb_trader)
+    else:
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Мы не предусмотрели данный запрос. Повторите попытку.")
+        
 
 @dp.message_handler(Text(equals='Статистика Профиля'))
 async def prof_stat(message: types.Message):
-    await bot.send_video(chat_id=message.from_user.id,
-                           video='https://c.mql5.com/1/78/open-uri20150119-12-2b4861__1.gif',
-                           caption='Статистика Профиля',
-                           reply_markup=ikb_trader_stat)
+    if trader_validate(message.from_user.id):
+        await bot.send_video(chat_id=message.from_user.id,
+                            video='https://c.mql5.com/1/78/open-uri20150119-12-2b4861__1.gif',
+                            caption='Статистика Профиля',
+                            reply_markup=ikb_trader_stat)
+    else:
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Мы не предусмотрели данный запрос. Повторите попытку.")
+    
