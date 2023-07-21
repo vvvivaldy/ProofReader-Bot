8e87358f15da674d90cde7e9cd2ea46e4f6f8456
mishkaqueryy <volokitin08@list.ru>
2023-07-22 02:24:19 +0300

    иморт ромин не записался

--- data/imports.py
+++ data/imports.py
@@ -6,7 +6,7 @@ from aiogram.types.message import ContentType
 from aiogram.dispatcher import FSMContext
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, ReplyKeyboardMarkup
 
-from data.config import DESCR, INSTRUCT, PREDOSTR, INFO, TRADER_HELP
+from data.config import DESCR, INSTRUCT, PREDOSTR, INFO, TRADER_HELP, SROKS
 from data.keyboards import kb_free, kb_instruct, kb_reg, kb_unreg, kb_profile, kb_admin, kb_black_list, kb_trader, kb_keys, kb_trader2, kb_leverage, kb_subscribe_on_trader, kb_confirmation, kb_contract
 from data.inline_keyboards import ikas, paykb, inl_kb_pr, inl_kb_status, ikk, ikst, ikb_period, ik_edit_api, ikb_quantity, ikb_trader_stat, kb_order
 from data.classes import Auth, Bl_Id_Trader, Bl_Id_User, UserDel, TraderStatus, UserStatus, SetUserSubscriptionStatus, EditApi, \
